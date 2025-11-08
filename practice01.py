07a2532c60a5a9ea9544145537a5e0ce0c8fe26e
Murali55-09 <muralidharkurer@gmail.com>
2025-11-08 23:02:51 +0530

    number +ve, -ve check

--- practice01.py
+++ practice01.py
@@ -1,1 +1,12 @@
 # Take a number and print whether it’s positive, negative, or zero.
+num = -7
+if(num > 0):
+    print("Positive Number")
+
+elif(num < 0):
+    print("Negative Number")
+
+else:
+    print("Zero")
+
+
