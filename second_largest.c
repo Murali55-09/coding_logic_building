ce64b5caeca07861270b0f66b0de4029da97648a
Murali55-09 <muralidharkurer@gmail.com>
2025-12-07 22:30:52 +0530

    third Largest Number

--- second_largest.c
+++ second_largest.c
@@ -4,26 +4,37 @@
 
 
 void main(){
-    int arr[] = {1, 3, 4, 1, 6, 8, 3, 1, 6};
+    int arr[] = {1, 3, 4, 1, 6, 8, 3, 1};
     int N  = sizeof(arr)/sizeof(arr[0]);
     int large = arr[0];
     int second_large = -1;
+    int third_large = -2;
 
     for(int i = 1 ; i < N; i++){
         if(arr[i] > large){
+            third_large = second_large;
             second_large = large;
             large = arr[i];
         }
 
         else if(arr[i] < large  && arr[i] > second_large){
+            third_large = second_large;
             second_large = arr[i];
+        }
+
+        else if(arr[i] < second_large  && arr[i] > third_large){
+            third_large = arr[i];
         }
     }
 
-    if(second_large == -1){
-        printf("No Numbers");
+    if(second_large == -1 ){
+        printf("No Second Numbers");
+    }
+    if(third_large == -2 ){
+        printf("No Third Numbers");
     }
     else{
-        printf("%d is the second Largest Number", second_large);
+        printf("%d is the second Largest Number\n", second_large);
+        printf("%d is the third Largest Number", third_large);
     }
 }
