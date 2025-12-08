a378c82808fd788169e2b87994d3d253e098ab95
Murali55-09 <muralidharkurer@gmail.com>
2025-12-08 08:07:26 +0530

    groupReverse of elements

--- reverse_array.c
+++ reverse_array.c
@@ -1,20 +1,53 @@
-// Reverse an array arr[]. Reversing an array means rearranging the elements such that the first element becomes the last, 
-// the second element becomes second last and so on.
+    // Reverse an array arr[]. Reversing an array means rearranging the elements such that the first element becomes the last, 
+    // the second element becomes second last and so on.
 
-#include<stdio.h>
+    #include<stdio.h>
 
-void main(){
-    int arr[] = {1, 2, 5, 1, 2, 21, 7, 2};
-    int N = sizeof(arr)/sizeof(arr[0]);
+    void reverseingroup(int group[], int k, int N){
+        for(int i = 0; i < N; i+=k){
+            int left = i;
+            int right = left + k - 1;
+            if(right >= N)
+                right = N - 1;
+            
+            while(left < right){
+                int temp = group[left];
+                group[left] = group[right];
+                group[right] = temp;
+                left++;
+                right--;
+            }
+        }
 
-    printf("Exact Array\n");
-    for (int i = 0; i < N; i++){
-        printf("%d ", arr[i] );
     }
 
-    printf("\nAfter Reverse\n");
-    for (int i = N - 1; i >= 0; i--){
-        printf("%d ", arr[i] );
+
+    int main(){
+        int arr[] = {1, 2, 5, 1, 2, 21, 7, 2};
+        int N = sizeof(arr)/sizeof(arr[0]);
+        int k = 4;
+
+        printf("Exact Array\n");
+        for (int i = 0; i < N; i++){
+            printf("%d ", arr[i] );
+        }
+
+        printf("\nAfter Reverse\n");
+        for (int i = N - 1; i >= 0; i--){
+            printf("%d ", arr[i] );
+        }
+
+        reverseingroup(arr, k, N);      //function call
+
+        printf("\nGroup reverse\n");
+        
+        for(int i = 0; i < N; i++){
+            printf("%d ", arr[i]);
+        }
     }
-}
+
+    // Given an array arr[] and an integer k, find the array after reversing every subarray of consecutive k elements in place. 
+    // If the last subarray has fewer than k elements, reverse it as it is. Modify the array in place, do not return anything.
+
+
 
