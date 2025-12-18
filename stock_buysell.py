209d86422b8d51ad79c18c82dda23b241681c4ec
Murali55-09 <muralidharkurer@gmail.com>
2025-12-18 23:24:28 +0530

    StockBuySell Problem solved

---
+++ stock_buysell.py
@@ -0,0 +1,14 @@
+
+def max_profit(prices):
+    profit = 0
+    
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i - 1]:
+            profit += prices[i] - prices[i - 1]
+    
+    return profit
+
+
+# Example
+prices = [100, 180, 260, 310, 40, 535, 695]
+print(max_profit(prices))  # Output: 865
