396f147e82ef1395f149ab575cebe50f5fac4cff
Murali55-09 <muralidharkurer@gmail.com>
2026-01-28 10:16:48 +0530

    Add logic to print the sum of first n natural numbers

--- practice06.py
+++ practice06.py
@@ -2,3 +2,8 @@
 n = 7
 for i in range(1, 11):
     print(f"{n} x {i} = {n * i}")   
+
+#  Print the sum of first n natural numbers.
+n = 10
+sum_natural = sum(range(1, n + 1))
+print(f"The sum of the first {n} natural numbers is: {sum_natural}")
